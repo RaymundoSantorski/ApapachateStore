dbba94ca9d4f057e87110144186941c4772253db
RaymundoSantorski <rayma9829@gmail.com>
2020-09-07 20:07:40 -0500

    funcion para eliminar items de carrito

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ var total=0
 
 function info(precio)
 {
-	var n = total.toString
-	return n
+	alert("El precio es $"+precio)
 }
 
 function add(precio, producto)
 {
+	var dv = document.createElement("div");
+	dv.id = producto;
+	dv.name = producto;
 	var pro = document.createElement("label");
+	var btn = document.createElement("button");
+	btn.textContent = "X"
+	btn.classList.add("boton");
+	btn.addEventListener('click', function(){
+		var eli = document.getElementsByName(producto);
+		var pa = document.getElementById(producto);
+		pa.remove(eli);
+		total=total-precio;
+		document.getElementById("total").innerHTML = "Total: $"+total;
+	});
+	document.querySelector(".items").appendChild(dv);
 	pro.innerHTML = producto + " $" + precio
-	document.querySelector(".items").appendChild(pro);
+	dv.appendChild(pro);
+	dv.appendChild(btn);
 	
 	total=total+precio
 }
