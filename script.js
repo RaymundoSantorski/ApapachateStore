e0031c0f4dcb133a24ea9726997229f854385818
RaymundoSantorski <rayma9829@gmail.com>
2020-09-08 14:36:04 -0500

    commit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function add(precio, producto)
 	var dv = document.createElement("div");
 	dv.id = producto;
 	dv.name = producto;
+	dv.classList.add("it");
 	var pro = document.createElement("label");
 	var btn = document.createElement("button");
 	btn.textContent = "X"
@@ -29,6 +30,11 @@ function add(precio, producto)
 	total=total+precio
 }
 
+function cerrar(){
+	var pop = document.getElementById("sucess");
+	pop.classList.remove('active');
+}
+
 function carrito()
 {
 	document.getElementById("total").innerHTML = "Total: $"+total;
@@ -40,4 +46,17 @@ function cerrarCarrito()
 {
 	var elemento = document.getElementById("overlay");
 	elemento.classList.remove('active');
+}
+
+function sucess(name){
+	document.getElementById("mensaje").innerHTML = "Gracias por tu compra "+name+"!";
+	var men = document.getElementById("sucess");
+	var over = document.getElementById("overlay");
+	men.classList.add("active");
+	over.classList.remove("active");
+	total = 0;
+	document.getElementById("total").innerHTML = null;
+	var elim = document.getElementsByClassName("it");
+	elim.remove();
+
 }
